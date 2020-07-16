ae61f292e7645fe3a52e1e21efe5776e1e343940
Sayana <nisayana23@yandex.ru>
2020-07-16 13:58:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,10 +13,13 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
+introduction_with_language("John", "JS")
+
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
+introduction_with_language_optional("Tom", "Python")
 
 
 
